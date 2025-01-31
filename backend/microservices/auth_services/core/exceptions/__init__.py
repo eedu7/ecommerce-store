d4b80a199462e3a3e82d2b095fe3c0b32441192f
eedu7 <mueedahmad067@gmail.com>
2025-01-31 08:10:37 +0500

    FEAT: Added exception status

---
+++ backend/microservices/auth_services/core/exceptions/__init__.py
@@ -0,0 +1,14 @@
+from .base import (BadRequestException, CustomException,
+                   DuplicateValueException, ForbiddenException,
+                   NotFoundException, UnauthorizedException,
+                   UnprocessableEntity)
+
+__all__ = [
+    "CustomException",
+    "BadRequestException",
+    "DuplicateValueException",
+    "ForbiddenException",
+    "NotFoundException",
+    "UnauthorizedException",
+    "UnprocessableEntity",
+]
