839c84b2bfc5cfc888687f2be8c43a0636bbdfbd
eedu7 <mueedahmad067@gmail.com>
2025-02-01 09:58:23 +0500

    FEAT: Separted the auth api from the user specific api and also only added the api end point without any logic the api end points are
    1.  Refresh Token
    2. Logout
    3. Change Password
    4. Forgot Password
    5. Reset Password
    6. Verify Email
    7. Resend Verification

--- backend/microservices/auth_services/api/v1/__init__.py
+++ backend/microservices/auth_services/api/v1/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
+from .auth import auth_router
 from .user import users_router
 
 v1_router = APIRouter()
 v1_router.include_router(users_router, prefix="/users")
+v1_router.include_router(auth_router, prefix="/auth")
---
+++ backend/microservices/auth_services/api/v1/auth/__init__.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+from .auth import router
+
+auth_router = APIRouter()
+auth_router.include_router(router, tags=["Authentication"])
+
+__all__ = ["auth_router"]
