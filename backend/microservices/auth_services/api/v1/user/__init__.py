5a2745d72f856ca5da856b0f41e4e29082a0c02f
eedu7 <mueedahmad067@gmail.com>
2025-02-01 10:03:37 +0500

    FEAT: Added the api endpoint to the user api end points

--- backend/microservices/auth_services/api/v1/user/__init__.py
+++ backend/microservices/auth_services/api/v1/user/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from .user import user_router
+from .user import router
 
 users_router = APIRouter()
-users_router.include_router(user_router, tags=["Users"])
+users_router.include_router(router, tags=["Users"])
 
 __all__ = ["users_router"]
