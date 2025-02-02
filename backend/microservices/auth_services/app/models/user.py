835d5a2abccdff1f05da2eaa6b5dd760cf242ea5
eedu7 <mueedahmad067@gmail.com>
2025-02-02 14:08:46 +0500

    Refactored the code, added the gender enum and migration

--- backend/microservices/auth_services/app/models/user.py
+++ backend/microservices/auth_services/app/models/user.py
@@ -17,15 +17,17 @@ class User(Base, TimeStampMixin, UserAuditMixin):
     uuid: Mapped[UUID] = mapped_column(
         UUID(as_uuid=True), default=uuid4, unique=True, nullable=False
     )
-    email: Mapped[str] = mapped_column(Unicode(255), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(Unicode(255), nullable=False)
-    username: Mapped[str] = mapped_column(Unicode(255), nullable=False, unique=True)
-    profile_image_url: Mapped[str] = mapped_column(Unicode(255), nullable=True)
-    phone_number: Mapped[str] = mapped_column(Unicode(255), nullable=True)
-    first_name: Mapped[str] = mapped_column(Unicode(255), nullable=True)
-    last_name: Mapped[str] = mapped_column(Unicode(255), nullable=True)
+    email: Mapped[str] = mapped_column(Unicode(320), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(Unicode(128), nullable=False)
+    username: Mapped[str] = mapped_column(Unicode(32), nullable=False, unique=True)
+    profile_image_url: Mapped[str] = mapped_column(Unicode(150), nullable=True)
+    phone_number: Mapped[str] = mapped_column(Unicode(20), nullable=True)
+    first_name: Mapped[str] = mapped_column(Unicode(50), nullable=True)
+    last_name: Mapped[str] = mapped_column(Unicode(50), nullable=True)
     date_of_birth: Mapped[date] = mapped_column(Date, nullable=True)
-    gender: Mapped[GenderEnum] = mapped_column(Enum(GenderEnum, create_type=False), nullable=True)
+    gender: Mapped[GenderEnum] = mapped_column(
+        Enum(GenderEnum, create_type=False), nullable=True
+    )
 
     def __repr__(self):
         return f"User(id={self.id}, uuid={self.uuid}, username={self.username}, email={self.email})"
