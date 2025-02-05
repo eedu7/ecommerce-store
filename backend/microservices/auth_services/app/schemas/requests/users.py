6c3e2720ccf41e8d38a25fb0c2d949d45b58972c
eedu7 <mueedahmad067@gmail.com>
2025-02-05 16:26:56 +0500

    MODIFY: replace validator from pydantic to field_validator, and used Field instead of constr

--- backend/microservices/auth_services/app/schemas/requests/users.py
+++ backend/microservices/auth_services/app/schemas/requests/users.py
@@ -2,39 +2,39 @@
 
 import re
 
-from pydantic import BaseModel, EmailStr, constr, validator
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 
 class RegisterUserRequest(BaseModel):
-    email: EmailStr
-    password: constr(min_length=8, max_length=64)
-    username: constr(min_length=3, max_length=64)
+    email: EmailStr = Field(..., examples=["john.doe@example.com"])
+    password: str = Field(..., min_length=8, max_length=64, examples=["Password@123"])
+    username: str = Field(..., min_length=3, max_length=64, examples=["JohnDoe"])
 
-    @validator("password")
+    @field_validator("password")
     def password_must_contain_special_characters(cls, v):
         if not re.search(r"[^a-zA-Z0-9]", v):
             raise ValueError("Password must contain special characters")
         return v
 
-    @validator("password")
+    @field_validator("password")
     def password_must_contain_numbers(cls, v):
         if not re.search(r"[0-9]", v):
             raise ValueError("Password must contain numbers")
         return v
 
-    @validator("password")
+    @field_validator("password")
     def password_must_contain_uppercase(cls, v):
         if not re.search(r"[A-Z]", v):
             raise ValueError("Password must contain uppercase characters")
         return v
 
-    @validator("password")
+    @field_validator("password")
     def password_must_contain_lowercase(cls, v):
         if not re.search(r"[a-z]", v):
             raise ValueError("Password must contain lowercase characters")
         return v
 
-    @validator("username")
+    @field_validator("username")
     def username_must_not_contain_special_characters(cls, v):
         if re.search(r"[^a-zA-Z0-9]", v):
             raise ValueError("Username must not contain special characters")
